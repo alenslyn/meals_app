ee86cbbd9f99f84586ae85164f3de4e0e7e4c46e
alenslyn <alenslyn@gmail.com>
2024-04-10 17:17:42 +0200

    feat: add icon button

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,12 +1,33 @@
 import { Text, View, ScrollView, Image, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import { MEALS } from "../data/dummy-data";
+import IconButton from "../components/IconButton";
 
-function MealDetailsScreen({ route }) {
+function MealDetailsScreen({ route, navigation }) {
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function headerButtonPress() {
+    console.log("Pressed");
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            title="Tap me"
+            onPress={headerButtonPress}
+          />
+        );
+      },
+    });
+  }, [navigation, headerButtonPress]);
 
   return (
     <ScrollView style={styles.root}>
